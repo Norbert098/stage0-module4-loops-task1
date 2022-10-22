dcce165955feed42ea9c494eccc711d73df0c584
Norbert Borek <norbertborek92@gmail.com>
2022-10-22 12:42:52 +0200

    done

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -3,7 +3,28 @@ package school.mjc.stage0.loops.task1;
 public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
+    public void repeatIsBad() {
+        final String REPETITION = "writing the same code doesn't have much impact, and it's also time consuming";
 
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
+        System.out.println(REPETITION);
     }
 }
